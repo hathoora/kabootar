50f8efa7e2d7c36290d093b9f6cf8adeb6741ded
Usman <attock@gmail.com>
2014-04-16 22:45:14 -0500

    adding foundation css assets

---
+++ kabootar/controller/baseController.php
@@ -0,0 +1,10 @@
+<?php
+namespace kabootar\controller
+{
+    use hathoora\controller\controller;
+
+    class baseController extends controller
+    {
+
+    }
+}
---
+++ kabootar/controller/defaultController.php
@@ -0,0 +1,12 @@
+<?php
+namespace kabootar\controller
+{
+    class defaultController extends baseController
+    {
+        public function index()
+        {
+            die('a');
+        }
+    }
+}
+        
